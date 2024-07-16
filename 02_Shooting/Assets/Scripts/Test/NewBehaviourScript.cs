8009d2d9ee7a8831809d7b32092c7ca95ed65f85
go2665 <77224762+go2665@users.noreply.github.com>
2024-07-16 13:49:18 +0900

    02_Shooting - 스프라이트 크기 설정, 한글표시

---
+++ 02_Shooting/Assets/Scripts/Test/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //한글한글   ㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 02_Shooting/Assets/Scripts/Test/Test01_Sprite.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01_Sprite : MonoBehaviour
+{
+    void Start()
+    {
+        // 시작할 때 한번 실행되는 함수(첫번째 업데이트 함수가 실행되기 직전에 한번 호출되는 함수)
+        Debug.Log("시작");
+    }
+
+    void Update()
+    {
+        // 프레임마다 매번 실행되는 함수
+        Debug.Log("업데이트");
+    }
+}
