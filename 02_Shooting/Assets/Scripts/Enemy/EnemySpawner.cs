7afd931ba0c0a18cf56edc6fa2c786d063c66369
go2665 <77224762+go2665@users.noreply.github.com>
2024-07-19 11:35:49 +0900

    02_Shooting - 실습조건

--- 02_Shooting/Assets/Scripts/Enemy/EnemySpawner.cs
+++ 02_Shooting/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 실습
+    // 1. 일정 시간 간격으로 적을 생성한다.
+    // 2. 생성되는 범위는 화면 오른쪽 밖(높이는 -4 ~ +4)
 }
