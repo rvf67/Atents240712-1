57039b687f156b4649c5f0dd0bd5bb51ef7e5705
go2665 <77224762+go2665@users.noreply.github.com>
2024-07-23 12:50:29 +0900

    02_Shooting - 싱글톤

---
+++ 02_Shooting/Assets/Scripts/Core/Factory.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Factory타입의 싱글톤
+public class Factory : Singleton<Factory>
+{
+}
---
+++ 02_Shooting/Assets/Scripts/Test/Test07_Singleton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test07_Singleton : TestBase
+{
+    private void Start()
+    {
+        // 객체 생성 방법
+        //TestSingleton test = new TestSingleton(); // public인 생성자가 없어서 new 불가능
+        TestClass test2 = new TestClass();
+        TestClass test3 = new TestClass(10);
+        //test2.staticNumber = 10;
+
+        TestClass.staticNumber = 10;
+        test2.TestPrint();
+        test3.TestPrint();
+
+        //TestSingleton.Instance.test = 20;
+
+    }
+
+    protected override void OnTest1(InputAction.CallbackContext context)
+    {
+        Debug.Log(Factory.Instance.gameObject);
+    }
+}
