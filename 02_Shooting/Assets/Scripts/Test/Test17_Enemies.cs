e3f42f02ec84c3759c6eab63e534275291520493
go2665 <77224762+go2665@users.noreply.github.com>
2024-08-01 12:31:29 +0900

    02_Shooting - 추가 적 테스트 용 씬 추가

---
+++ 02_Shooting/Assets/Scripts/Test/Test17_Enemies.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test17_Enemies : TestBase
+{
+    // 적 추가
+    // 1. EnemyBonus : 죽을 때 파워업 아이템을 생성. 스폰되었을 때 오른쪽 끝에 등장한 후 잠시 대기하고 그 이후에 다시 움직인다.
+    // 2. EnemyCurve : 커브를 도는 움직임을 한다. 위쪽에 스폰되었으면 좌회전, 아래쪽에 스폰되었으면 우회전
+    // 3. 멀티스포너에 적용하기
+    
+}
